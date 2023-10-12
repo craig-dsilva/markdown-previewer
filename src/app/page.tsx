7224905f8d99c10d47974b1b972b065dc46e4562
craig-dsilva <craig.s.dsilva@gmail.com>
2023-10-12 16:57:16 +0100

    render editor and preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useState } from "react";
+import Markdown from "react-markdown";
 import styles from "./page.module.css";
 
 const Home = () => {
-  return <main className={styles.main}></main>;
+const [preview, setPreview] = useState("")
+
+  return <main className={styles.main}>
+    <textarea id={styles.editor} value={preview} onChange={e => setPreview(e.target.value)}></textarea>
+      <div id={styles.preview}>
+        <Markdown>{preview}</Markdown>
+      </div>
+  </main>;
 };
 
 export default Home;
