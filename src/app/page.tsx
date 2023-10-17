08179bcab441e20d2ecb317891b94cc240d221f5
craig-dsilva <craig.s.dsilva@gmail.com>
2023-10-17 11:00:36 +0100

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useState } from "react";
 import Markdown from "react-markdown";
+import { defaultTextInEditor } from "./defaultTextInEditor";
 import styles from "./page.module.css";
 
 const Home = () => {
-const [preview, setPreview] = useState("")
+const [preview, setPreview] = useState(defaultTextInEditor)
 
   return <main className={styles.main}>
     <textarea id={styles.editor} className={styles.column} value={preview} onChange={e => setPreview(e.target.value)}></textarea>
