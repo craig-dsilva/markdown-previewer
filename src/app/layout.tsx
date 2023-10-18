bc9c5bd7e762672355ab950c202dfe41ea8793c2
craig-dsilva <craig.s.dsilva@gmail.com>
2023-10-18 16:04:20 +0100

    add comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <link rel="icon" href="/markdown.svg" sizes="any" />
     </html>
   )
 }
